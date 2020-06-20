e3f3ae471912b881fc99611719f32cb0c186d4cd
FrostiaKk <60479225+FrostiaKk@users.noreply.github.com>
2020-06-20 18:42:01 +0200

    Initial commit

---
+++ Bomb It/Bomb It/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Bomb_It
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new Bomb_It.MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Bomb It/Bomb It/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace Bomb_It
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        static string bomb = new Random().Next(1, 5).ToString();
+        static int Scores = 0;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void ButtonClicked(object sender, EventArgs e)
+        {
+            Button button = sender as Button;
+
+            if (button.Text == bomb)
+            {
+                Vibration.Vibrate();
+                await DisplayAlert("Bomba wybuchła", "Koniec gry", "Zagraj jeszcze raz");
+                bomb = new Random().Next(1, 4).ToString();
+            }
+            else
+            {
+                Scores += 1;
+                await DisplayAlert("Bomba rozbrojona", "Wynik: " + Scores, "Kontynuuj");
+                bomb = new Random().Next(1, 4).ToString();
+            }
+        }
+    }
+}
